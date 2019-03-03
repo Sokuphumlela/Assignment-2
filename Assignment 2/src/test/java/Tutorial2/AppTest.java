904e39bd0cfd3731254e5877b21e1bb1ffc16c07
Sokuphumlela <nombila.s@gmail.com>
2019-03-03 07:21:34 +0200

    Add files via upload

---
+++ Assignment 2/src/test/java/Tutorial2/AppTest.java
@@ -0,0 +1,30 @@
+package Tutorial2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+     List<String> list = List.of("a", "b", "c");
+
+     Set<String> set = Set.of("a", "b", "c");
+
+     Map<String, Interger> mapImmediate = Map.of(
+             "one", 1,
+             "two", 2,
+             "three" 3,);
+     )
+}
